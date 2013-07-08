17dcb2dbe73630918fb0ce4a8cae6db9b4baefb7
Andy Quin <andy@sharpfrog.com>
2013-07-08 08:54:35 +0100

    inverse index last question to do

--- matrix/inverse index lab/dictutil.py
+++ matrix/inverse index lab/dictutil.py
@@ -1,7 +1,7 @@
 ## Task 2
-def dict2list(dct, keylist): pass
+def dict2list(dct, keylist): return [ dct[x] for x in keylist if x in dct ]
 
-def list2dict(L, keylist): pass
+def list2dict(L, keylist): return { x:y for (x,y) in set(zip(keylist, L)) }
 
 ## Task 3
 def listrange2dict(L):
@@ -11,5 +11,5 @@ def listrange2dict(L):
 
     You can use list2dict or write this from scratch
     """
-    pass
+    return list2dict(L, range(len(L)))
 
--- matrix/inverse index lab/inverse_index_lab.py
+++ matrix/inverse index lab/inverse_index_lab.py
@@ -8,11 +8,11 @@ def movie_review(name):
     Output: a string (one of the review options), selected at random using randint
     """
     review_options = ["See it!", "A gem!", "Ideological claptrap!"]
-    return ...
+    return review_options[randint(0,len(review_options)-1)]
 
 ## Tasks 2 and 3 are in dictutil.py
 
-## Task 4    
+## Task 4
 def makeInverseIndex(strlist):
     """
     Input: a list of documents as strings
@@ -22,7 +22,16 @@ def makeInverseIndex(strlist):
     Note that to test your function, you are welcome to use the files stories_small.txt
       or stories_big.txt included in the download.
     """
-    return ...
+    inverseIndex = {}
+    for i in range(len(strlist)):
+        docWords = strlist[i].split()
+        for j in range(len(docWords)):
+            word = docWords[j]
+            if not word in inverseIndex:
+                inverseIndex[word] = set()
+            inverseIndex[word].add(i)
+
+    return inverseIndex
 
 ## Task 5
 def orSearch(inverseIndex, query):
@@ -30,7 +39,14 @@ def orSearch(inverseIndex, query):
     Input: an inverse index, as created by makeInverseIndex, and a list of words to query
     Output: the set of document ids that contain _any_ of the specified words
     """
-    return ...
+
+    matches = set()
+    for i in query:
+        if i in inverseIndex:
+            matches = matches | inverseIndex[i]
+    return matches
+
+#    return {inverseIndex[x] for x in query if x in inverseIndex}
 
 ## Task 6
 def andSearch(inverseIndex, query):
@@ -38,4 +54,9 @@ def andSearch(inverseIndex, query):
     Input: an inverse index, as created by makeInverseIndex, and a list of words to query
     Output: the set of all document ids that contain _all_ of the specified words
     """
-    return ... 
+
+    matches = set()
+    for i in query:
+        if i in inverseIndex:
+            matches = matches & inverseIndex[i]
+    return matches
